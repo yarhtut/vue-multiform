ce673f46f4a6d561f0cdfca51781c2a3f87da088
yarhtut <yar@boost.co.nz>
2018-05-05 20:12:14 +1200

    contentful client in plugins module

--- app/javascript/packs/profile.ts
+++ app/javascript/packs/profile.ts
@@ -10,7 +10,6 @@ Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: 'history',
-  base: __dirname,
   routes: [
     { path: '/', component: Form },
     { path: '/post/:id', component: Page },
--- app/javascript/pages/page.vue
+++ app/javascript/pages/page.vue
@@ -4,8 +4,8 @@
     <div v-if="page.error" class="error">
       {{ page.error }}
     </div>
-    <transition name="slide">
-    <div v-if="page.parts" class="content" :key="page.post.slug">
+    <transition name="fade">
+    <div v-show="page.parts" class="content">
       <h1>ContentFul with <em>JS SDK</em></h1>
       <h2>{{ page.post.title }}</h2>
       <vue-markdown>{{ page.parts.body }}</vue-markdown>
@@ -17,29 +17,26 @@
 <script lang='ts'>
 import Vue from 'vue'
 import Component from 'vue-class-component'
-import {createClient} from 'contentful'
 import VueMarkdown from 'vue-markdown'
+import client from '../plugins/contentful.js'
 
 Vue.use(VueMarkdown)
-
-const client = createClient({
-  space: 'lwaqndzrnasi',
-  accessToken: '95a34b52fe0658bde9433bdd78b0194b0a68f16b325328a5d9e6764895972d74'
-})
 
 @Component({
   components: {
     VueMarkdown
   }
 })
+
 export default class Page extends Vue {
 
   page = {
     loading: false,
     post: null,
     error: null,
-    parts: null,
+    parts: null
   }
+
 
   created() {
     this.fetchPage()
@@ -52,22 +49,24 @@ export default class Page extends Vue {
   fetchPage() {
     this.page.error = this.page.post = null
     this.page.loading = true
+    this.previousId = this.$route.params.id
 
     client.getEntry(this.$route.params.id)
     .then((entry) =>  {
       this.page.post = entry.fields
 
+
       const partId = entry.fields.parts[0].sys.id
 
       client.getEntry(partId)
-      .then((child) => {
-        this.page.parts = child.fields
+      .then((part) => {
+        this.page.parts = part.fields
       })
+
       this.page.loading = false
     })
     .catch((err) => console.log(err))
   }
-
 }
 </script>
 
