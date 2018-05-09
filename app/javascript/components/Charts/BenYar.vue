60699268944d7177a8df38b2e33fe7f0d328cdab
yarhtut <yar@boost.co.nz>
2018-05-10 11:46:28 +1200

    add child component example

---
+++ app/javascript/components/Charts/BenYar.vue
@@ -0,0 +1,14 @@
+
+<template>
+  <div class='row'>
+    <div class='columns'>
+      <h1>Ben & Yar</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
--- app/javascript/components/Charts/Charts.vue
+++ app/javascript/components/Charts/Charts.vue
@@ -3,16 +3,19 @@
     <div class='columns'>
       <h1>Chart with <em>Vue-Charts</em></h1>
       <line-chart> </line-chart>
+    <ben-yar></ben-yar>
     </div>
   </div>
 </template>
 
 <script>
 import LineChart from './LineChart.vue';
+import BenYar from './BenYar.vue';
 
 export default {
   components: {
-    LineChart
+    LineChart,
+    BenYar
   }
 }
 </script>
